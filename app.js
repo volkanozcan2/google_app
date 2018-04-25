560c308ba0acbc23609f0109f883649f63581c07
negerekvar <volqii@googlemail.com>
2018-04-25 13:00:55 +0300

    env variable set

--- app.js
+++ app.js
@@ -7,18 +7,14 @@ var indexRouter = require('./routes/index');
 var hasher = require('./routes/hash');
 var app = express();
 var io = app.io = require("socket.io")();
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/hasher', hasher);
 let connectCounter = 0;
@@ -31,24 +27,19 @@ io.on("connect", function(socket) {
     socket.on("disconnect", () => {
         connectCounter--;
         io.emit("counter", connectCounter);
-
     })
 });
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
-
 module.exports = app;
